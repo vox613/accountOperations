2518e570f757651b371ba5f771a2ad6df545c2cb
ai.aleksandrov <vox613@gmail.com>
2020-01-13 14:46:28 +0300

    Empty project

---
+++ src/main/java/com/iteco/a/alexandrov/accountOperations/AccountOperationsApplication.java
@@ -0,0 +1,13 @@
+package com.iteco.a.alexandrov.accountOperations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountOperationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountOperationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iteco/a/alexandrov/accountOperations/AccountOperationsApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.iteco.a.alexandrov.accountOperations;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class AccountOperationsApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
